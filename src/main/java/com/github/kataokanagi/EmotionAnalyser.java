4b8fd79282191cdd9ce4c61dce6102356d20fad3
KataokaNagi <al18036@shibaura-it.ac.jp>
2021-01-16 19:21:01 +0900

    Refactor RmotionAnalyser.java (Clear test)

--- src/main/java/com/github/kataokanagi/EmotionAnalyser.java
+++ src/main/java/com/github/kataokanagi/EmotionAnalyser.java
@@ -1,61 +1,196 @@
+/**
+* @file EmotionAnalyser
+* @brief     Analyse positive level from the param comment
+* @auther    Kataoka Nagi, xqq
+* @date      2021-01-12 10:53:20
+* $Version   1.0
+* $Revision  1.2
+* @par       Refactored
+* @see      『Javaで簡単に感情分析する方法』 https://spjai.com/emotion-analysis/
+*/
+
 package com.github.kataokanagi;
 
 import com.github.kataokanagi.utils.Log;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EmotionAnalyser {
 
-    private static final String TAG = "EmotionAnalyser";
+  private static final String TAG = "EmotionAnalyser";
 
-    private Map<String, Integer> sentiMap = new HashMap<>();
+  private final String POLAR_DICTIONARY_PATH = "pn.csv.m3.120408.trim"; // ! @auther Inui-Okazaki Laboratory,
+                                                                        // Tohoku University
+  private Map<String, Integer> positiveLevelMap = new HashMap<>();
 
-    public EmotionAnalyser(String dictionaryPath) throws IOException {
-        File file = new File(dictionaryPath);
+  /**
+   * @fn EmotionAnalyser
+   * @brief Map word to it's positive level
+   */
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
+  public EmotionAnalyser() throws IOException {
+    try {
+      File dictionaryFile = new File(POLAR_DICTIONARY_PATH);
+      String dictionaryLine = null;
+      BufferedReader br = new BufferedReader(new FileReader(dictionaryFile));
 
-        String line = null;
+      // Read each lines of polar dictionary
+      while ((dictionaryLine = br.readLine()) != null) {
 
-        while ((line = br.readLine()) != null) {
-            String[] split = line.split("\t");
-            if (split.length > 1) {
-                String emotion = split[1].trim(); // p or e or n
-                int sentiScore = 0;
-                if (emotion.equals("p")) {
-                    sentiScore = 1; // pの場合+1
-                } else if (emotion.equals("n")) {
-                    sentiScore = -1; // nの場合-1
-                }
-                sentiMap.put(split[0].trim(), sentiScore); // 単語とスコアの格納
-            }
+        // Example: "２，３日" e 〜である・になる（状態）客観
+        String[] wordPolarInfo = dictionaryLine.split("\t");
+
+        // Catch dictionary exception
+        if (wordPolarInfo.length > 1) {
+          continue;
         }
 
-        br.close();
-    }
-
-    public double getPositiveLevel(String comment) {
-        return 0.0;
-    }
-
-    private List<String> getCommentSurfaceList() {
-        return null;
-    }
-
-    private double evalFunc(int positiveCount, int negativeCount) {
-        double eval;
-        if (positiveCount + negativeCount != 0) {
-            eval = (double) (positiveCount - negativeCount) / (positiveCount + negativeCount);
-        } else {
-            eval = 0.0;
+        // Parse word's positive level to int in {-1 , 0, 1}
+        int wordPositiveLevel;
+        String emotionTag = wordPolarInfo[1].trim();
+        switch (emotionTag) {
+          case "p": // positive word
+            wordPositiveLevel = 1;
+          case "e": // neutral word
+            wordPositiveLevel = 0;
+          case "n": // negative word
+            wordPositiveLevel = -1;
+          default:
+            wordPositiveLevel = -999;
+            Log.w(TAG, "Unexpected emotionTag: " + emotionTag);
         }
-        Log.d(TAG, "eval = " + eval);
-        return eval;
+
+        // Map word to it's positive level
+        this.positiveLevelMap.put(wordPolarInfo[0].trim(), wordPositiveLevel);
+      }
+
+      br.close();
+
+    } catch (IOException e) {
+      // BufferedReader exceptions of open, read, close
+      throw new IOException(e.getMessage());
     }
+  }
+
+  /**
+   * @fn getPositiveLevel
+   * @param comment : Target of the analysis
+   * @return commentPositiveLevel : of the param Comment in all double number
+   */
+  public double getPositiveLevel(String comment) throws IOException {
+    // In param comment,
+    int positiveCnt = 0;
+    int negativeCnt = 0;
+    int neutralCnt = 0;
+
+    // Example: [吾輩, は, 猫, で, ある] <- 吾輩は猫である
+    List<String> surfaceList = this.getCommentSurfaceList(comment);
+
+    // Count words according to each positive level if polar dictionary contains
+    // surface words
+    for (String surface : surfaceList) {
+      if (positiveLevelMap.containsKey(surface)) {
+        int wordPositiveLevel = positiveLevelMap.get(surface);
+        switch (wordPositiveLevel) {
+          case 1:
+            ++positiveCnt;
+            break;
+          case 0:
+            ++neutralCnt;
+          case -1:
+            ++negativeCnt;
+            break;
+          default:
+            Log.w(TAG, "Unexpected wordPositiveLevel: " + wordPositiveLevel);
+        }
+      }
+    }
+
+    Log.d(TAG, "positiveCnt = " + positiveCnt);
+    Log.d(TAG, "neutralCnt = " + neutralCnt);
+    Log.d(TAG, "negativeCnt = " + negativeCnt);
+
+    double commentPositiveLevel = this.evalFunc(positiveCnt, negativeCnt);
+    return commentPositiveLevel;
+  }
+
+  /**
+   * @fn getCommentSurfaceList
+   * @return surfaceList : Word of surface type (ex. 吾輩は猫である->[吾輩, は, 猫, で, ある] )
+   * @auther Kataoka Nagi, Sugimoto Prof.
+   */
+  private List<String> getCommentSurfaceList(String comment) throws IOException {
+    List<String> surfaceList = new ArrayList<String>();
+    final int SURFACE_IDX = 0;
+
+    // Run MeCab
+    Process MeCabProcess = Runtime.getRuntime().exec("mecab");
+
+    try {
+      // Send comment to MeCab
+      BufferedReader br = new BufferedReader(new InputStreamReader(MeCabProcess.getInputStream()));
+      PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(MeCabProcess.getOutputStream())));
+      pw.println(comment);
+      pw.flush();
+
+      // Receive morphological analysis results from MeCab
+      // Example: で\t助動詞,*,*,*,特殊・ダ,連用形,だ,デ,デ
+      String MeCabOutputLine;
+      while ((MeCabOutputLine = br.readLine()) != null) {
+        if (MeCabOutputLine.equals("EOS")) {
+          break;
+        }
+
+        // Get word of surface type (first word of MeCabOutputLine)
+        String surface = MeCabOutputLine.split(",")[SURFACE_IDX].split("\t")[0];
+        surfaceList.add(surface);
+
+        Log.v(TAG, "MeCabOutputLine = " + MeCabOutputLine);
+        Log.v(TAG, "surface = " + surface);
+      }
+
+      br.close();
+      pw.close();
+      MeCabProcess.destroy();
+
+    } catch (IOException e) {
+      throw new IOException(e.getMessage());
+      // e.printStackTrace();
+    }
+
+    return surfaceList;
+  }
+
+  /**
+   * @fn evalFunc
+   * @param positiveCnt : Number of the positive words in a comment
+   * @param negativeCnt : Number of the negative words in a comment
+   * @return eval : Evaluation of positive level in a comment
+   * @see 『Web上の誹謗中傷を表す文の自動抽出 』p.42
+   *      https://www.slideshare.net/jnlp/11thesis-ishisaka-32213284
+   */
+  private double evalFunc(int positiveCnt, int negativeCnt) {
+    double eval;
+
+    // Escape of division by zero
+    if (positiveCnt + negativeCnt != 0) {
+      eval = (double) (positiveCnt - negativeCnt) / (positiveCnt + negativeCnt);
+    } else {
+      eval = 0.0;
+    }
+
+    Log.d(TAG, "eval = " + eval);
+    return eval;
+  }
 }
--- src/main/java/com/github/kataokanagi/ReplyGenerator.java
+++ src/main/java/com/github/kataokanagi/ReplyGenerator.java
@@ -1,43 +1,44 @@
 package com.github.kataokanagi;
 
 import com.github.kataokanagi.utils.Log;
-import com.github.kataokanagi.GetStrPstvLv;
+import com.github.kataokanagi.EmotionAnalyser;
+
+import java.io.IOException;
 
 public class ReplyGenerator {
 
-    private static final String TAG = "ReplyGenerator";
-    private GetStrPstvLv getStrPstvLv;
+  private static final String TAG = "ReplyGenerator";
+  private EmotionAnalyser emotionAnalyser;
 
-    public ReplyGenerator() {
-        getStrPstvLv = new GetStrPstvLv();
+  public ReplyGenerator() throws IOException {
+    emotionAnalyser = new EmotionAnalyser();
+  }
+
+  public String generate(String comment) throws IOException {
+    String reply;
+    double positiveLevel = emotionAnalyser.getPositiveLevel(comment);
+
+    if (positiveLevel == 1.0) { // positiveLevel \in [-1,1]
+      reply = "すっごーい！";
+    } else {
+      int val = (int) ((positiveLevel + 1) * 10) % 20;
+      if (val >= 0 && val <= 3) {
+        reply = "ワロス";
+      } else if (val >= 4 && val <= 6) {
+        reply = "あし";
+      } else if (val >= 7 && val <= 9) {
+        reply = "わろし";
+      } else if (val >= 10 && val <= 12) {
+        reply = "よろし";
+      } else if (val >= 13 && val <= 15) {
+        reply = "よし";
+      } else { // case 16, 17, 18, 19
+        reply = "ヨシ！";
+      }
     }
 
-    public String generate(String comment) {
-        String reply;
-        double positiveLevel = getStrPstvLv.getLv(comment);
-
-
-        if (positiveLevel == 1.0) {  // positiveLevel \in [-1,1]
-            reply = "すっごーい！";
-        } else {
-            int val = (int) ((positiveLevel + 1) * 10) % 20;
-            if (val >= 0 && val <= 3) {
-                reply = "ワロス";
-            } else if (val >= 4 && val <= 6) {
-                reply = "あし";
-            } else if (val >= 7 && val <= 9) {
-                reply = "わろし";
-            } else if (val >= 10 && val <= 12) {
-                reply = "よろし";
-            } else if (val >= 13 && val <= 15) {
-                reply = "よし";
-            } else {  // case 16, 17, 18, 19
-                reply = "ヨシ！";
-            }
-        }
-
-        Log.v(TAG, comment + " -> " + reply);
-        return reply;
-    }
+    Log.v(TAG, comment + " -> " + reply);
+    return reply;
+  }
 
 }
